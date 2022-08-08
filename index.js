2895fadbfe3d5276deabd38780c1f9aa9bc19c1e
Noah King <noahking123@yahoo.com>
2022-08-08 14:51:04 -0400

    commit tree 5971df9771467b1c5d8d14fae9db9575c05f0353

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
 
-// Write your code here
+let random;
+random = Math.floor(Math.random()*10)
+
+
+let num3 = 16
+let num4 = 6
+let mod = num3 % num4;
+
+let max = (1,2,5,15,20);
+Math.max(max);
